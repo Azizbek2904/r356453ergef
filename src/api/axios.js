c3c40562a69095a30f5a7d8c927fa5f38af36e77
Jafaruz914 <programmeraziz5@gmail.com>
2025-10-21 10:08:03 +0500

    fix: set backend server IP

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
-  headers: { "Content-Type": "application/json" },
+    baseURL: "http://207.154.227.250:8080",
+    headers: { "Content-Type": "application/json" },
 });
+
 
 // ✅ 1. Tokenni har bir so‘rovga qo‘shish
 api.interceptors.request.use((config) => {
@@ -21,7 +22,7 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       const oldToken = localStorage.getItem("token");
       try {
-        const res = await axios.post("http://localhost:8080/auth/refresh-token", { token: oldToken });
+          const res = await axios.post("http://207.154.227.250:8080/auth/refresh-token", { token: oldToken });
         const newToken = res.data.token;
         localStorage.setItem("token", newToken);
         error.config.headers["Authorization"] = `Bearer ${newToken}`;
